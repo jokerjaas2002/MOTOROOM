791263fba1ed505d63de3cf73bc66989ec357d85
jokerjaas2002 <joelacosta712@gmail.com>
2025-02-10 22:57:56 -0400

    Feat: Add the new code

--- index.php
+++ index.php
@@ -9,20 +9,33 @@
 <body>
     <?php include 'includes/header.php'; ?>
     <main>
+    
         <h1>Bienvenido a MOTOROOM</h1>
         <p>Tu tienda de venta y mantenimiento de motocicletas.</p>
         <img src="Texto del párrafo (1).gif" alt="Logo de MOTOROOM" class="logo-animado">
-        <section>
-            <h2>Nuestros Servicios</h2>
-            <p>Ofrecemos una amplia gama de servicios para tu motocicleta, desde mantenimiento hasta reparaciones.</p>
-            <a href="servicios.php">Ver Servicios</a>
-        </section>
-        <section>
-            <h2>Productos Destacados</h2>
-            <p>Explora nuestra selección de motocicletas y accesorios.</p>
-            <a href="productos.php">Ver Productos</a>
-        </section>
+        
+        <div class="contenedor">
+    <section>
+        <a href="productos.php">
+            <div class="image-container">
+                <img src="bmw-07.jpg" alt="Moto" class="imagen-producto">
+                <h2>Productos Destacados</h2>
+            </div>
+        </a>
+    </section>
+
+    <section>
+        <a href="servicios.php">
+            <div class="image-container">
+                <img src="Mecanica.jpg" alt="Mecanica" class="imagen-producto">
+                <h2>Nuestros Servicios</h2>
+            </div>
+        </a>
+    </section>
+</div>
     </main>
+
+   
     <?php include 'includes/footer.php'; ?>
 </body>
 </html>
